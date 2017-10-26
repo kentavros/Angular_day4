85c79beb4aaef90490cb4806f447624f8a128b15
student <student@GFL-1465.ad.geeksforless.net>
2017-10-26 20:32:03 +0300

    2

--- app/main.js
+++ app/main.js
@@ -1,1 +1,33 @@
-angular.module('MyApp', [])
+var app = angular.module('app', []);
+
+app.directive('username', function($q, $timeout) {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      var usernames = ['Sergey', 'Tolik', 'Vasya', 'Fedia'];
+
+      ctrl.$asyncValidators.username = function(modelValue, viewValue) {
+
+        if (ctrl.$isEmpty(modelValue)) {
+          // consider empty model valid
+          return $q.resolve();
+        }
+
+        var def = $q.defer();
+
+        $timeout(function() {
+          // Mock a delayed response
+          if (usernames.indexOf(modelValue) === -1) {
+            // The username is available
+            def.resolve();
+          } else {
+            def.reject();
+          }
+
+        }, 1000);
+
+        return def.promise;
+      };
+    }
+  };
+});
